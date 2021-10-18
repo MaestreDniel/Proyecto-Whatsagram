c4ecec2de1a2df32a51673f39df40fb76989a045
Daniel Maestre <dmaestre@cifpfbmoll.eu>
2021-10-18 19:55:26 +0200

    Avances en la interfaz (no perfeccionada)

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 console.log("Estructura de proyecto");
+
+function hola() {
+    // document.getElementById("hola").innerHTML = "Hola";
+}
+
+function imprimirTecla(valor) {
+    document.getElementById("").innerHTML += valor;
+}
+
+document.getElementsByClassName("a").onclick = function() {
+    imprimirTecla(this.class); 
+    // O si no puedo, hacerlo con las id y getElmById, y también se puede poner directamente 
+    // la letra pero el nivel de abstracción es menor
+}
+
+document.getElementsByClassName("b").onclick = function() {
+    imprimirTecla(this.class); // O si no puedo, hacerlo con las id
+}
