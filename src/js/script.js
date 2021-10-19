8f9dbbc0f3a9d0c3e78bb76b409d2eafd89d03b7
Daniel Maestre <dmaestre@cifpfbmoll.eu>
2021-10-19 23:14:45 +0200

    Interfaz funcional para móvil Galaxy

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,21 @@
-console.log("Estructura de proyecto");
 
-function hola() {
-    // document.getElementById("hola").innerHTML = "Hola";
+document.getElementById("send").addEventListener("click", function() {
+    enviarMsg();
+});
+
+const teclado = document.getElementsByClassName("teclado")[0].addEventListener("click", function() {
+    imprimirTecla(teclado);
+});
+
+function imprimirTecla(teclado) {
+    //document.getElementById("a").value;
+    document.getElementById("textarea").innerHTML += teclado;
 }
 
-function imprimirTecla(valor) {
-    document.getElementById("").innerHTML += valor;
-}
-
-document.getElementsByClassName("a").onclick = function() {
-    imprimirTecla(this.class); 
-    // O si no puedo, hacerlo con las id y getElmById, y también se puede poner directamente 
-    // la letra pero el nivel de abstracción es menor
-}
-
-document.getElementsByClassName("b").onclick = function() {
-    imprimirTecla(this.class); // O si no puedo, hacerlo con las id
+function enviarMsg() {
+    let mensaje = document.getElementById("textarea").value;
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(mensaje));
+    let divcont = document.getElementsByClassName("msg")[0];
+    divcont.appendChild(div);
 }
